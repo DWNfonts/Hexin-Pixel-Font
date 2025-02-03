5a3ce68115258d4c19adf8bd8018a0b0d003e4d0
diaowinner <diaowinner@qq.com>
2025-02-03 15:54:28 +0800

    Add timer in build script.

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -1,6 +1,9 @@
+$timer = [Diagnostics.Stopwatch]::StartNew()
 Set-Location $PSScriptRoot
 Remove-Item ../Output/HexinPixelFont.otf -Force # bug?
 New-Item ../Output/Glyphs -ItemType Directory -Force
 python FetchGlyphs.py
 python BuildChar.py
 python BuildFont.py fun
+$timer.Stop()
+Write-Host "Done in $($timer.Elapsed.TotalSeconds) seconds."
