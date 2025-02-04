b7b3c78961b6d34b72cf8b7eb218fe4d1322b755
diaowinner <diaowinner@qq.com>
2025-02-04 20:32:10 +0800

    Rename MakeImage to MakeCCB, add FetchCCB in the build script.

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -2,8 +2,9 @@ $timer = [Diagnostics.Stopwatch]::StartNew()
 Set-Location $PSScriptRoot
 Remove-Item ../Output/HexinPixelFont.otf -Force # bug?
 New-Item ../Output/Glyphs -ItemType Directory -Force
-python FetchGlyphs.py
 python BuildChar.py
+python FetchGlyphs.py
+python FetchCCB.py
 python BuildFont.py fun
 $timer.Stop()
 Write-Host "Done in $($timer.Elapsed.TotalSeconds) seconds."
